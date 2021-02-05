40ded0f60e03706f02a0a1092a211701a45ac708
Jiahao Liu <jobsrefer2020@gmail.com>
2021-02-04 20:01:05 -0800

    URL_FOR function

--- hello.py
+++ hello.py
@@ -6,9 +6,20 @@ app = Flask(__name__)
 def hello_world():
     return 'I love you Xiaofan!'
 
+@app.route('/admin')
+def hello_admin():
+    return f'Hello admin. You can edit this page'
+
+@app.route('/<guest>')
+def hello_guest(guest):
+    return f'Hello {guest} as guest'
+
 @app.route('/<name>')
 def hello_name(name):
-    return f'Hello {name}! This is Jiahao\'s website.'
-
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest = name))
+        
 if __name__=="__main__":
     app.run(debug = True)
