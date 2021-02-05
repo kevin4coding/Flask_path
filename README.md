5b867356decc3988d9508e13327194100d8b010d
Jiahao Liu <jobsrefer2020@gmail.com>
2021-02-04 19:53:39 -0800

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Flask Learning Path
+
+## How to run Flask web server on real time
+**Introduction**
+Flask is using Werkzeug library for allowing 'auto reload' function available that can be enabled by doing one of the following:
+*app.run(debug=True)*
+or
+*app.debug=True*
--- hello.py
+++ hello.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'I love you Xiaofan!'
-    
+
 @app.route('/<name>')
 def hello_name(name):
-    return f'Hello {name}! This is Jiahao\'s website'
+    return f'Hello {name}! This is Jiahao\'s website.'
 
 if __name__=="__main__":
     app.run(debug = True)
