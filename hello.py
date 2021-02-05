fc42362565f270062cbf2114df6fda5289e253b8
Jiahao Liu <jobsrefer2020@gmail.com>
2021-02-04 19:45:04 -0800

    Url learning

--- hello.py
+++ hello.py
@@ -3,6 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
     return 'I love you Xiaofan!'
+    
+@app.route('/<name>')
+def hello_name(name):
+    return f'Hello {name}! This is Jiahao\'s website'
+
+if __name__=="__main__":
+    app.run(debug = True)
